8648e90ebeb6a53965183416da7c10759c752bc3
Deepak Kamat <dkamat.001@gmail.com>
2022-03-05 22:43:15 +0530

    feat: fetching query for playlist

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import "./styles.css";
 import { Sidebar, PlayerList, MusicPlayer } from "./components";
+import { useQuery, gql } from "@apollo/client";
+
+const PLAYLIST_QUERY = gql`
+  query ExampleQuery($playlistId: Int!) {
+    getSongs(playlistId: $playlistId) {
+      _id
+      title
+      photo
+      url
+      duration
+      artist
+    }
+  }
+`;
 
 export default function App() {
+  const { data, loading, error } = useQuery(PLAYLIST_QUERY, {
+    variables: { playlistId: 1 },
+  });
+  // console.log(data);
   return (
     <div className="app">
       <Sidebar />
-      <PlayerList />
+      <PlayerList musicData={data} />
       <MusicPlayer />
     </div>
   );
--- src/components/playerList/PlayerList.js
+++ src/components/playerList/PlayerList.js
@@ -1,21 +1,28 @@
 import "./PlayerList.css";
 import Album from "./album.png";
+import { useState } from "react/cjs/react.production.min";
 
-export const PlayerList = () => {
+export const PlayerList = ({ musicData }) => {
+  const [data, setData] = useState(musicData);
+  console.log(musicData);
   return (
     <div className="playerList">
       <div className="playerList-searchbar">
         <h2>For You</h2>
         <input type="text" placeholder="Search Song, Artist" />
       </div>
-      <div className="playerList-playlist">
-        <img alt="Album Poster" src={Album} />
-        <div className="playerList-album-title">
-          <h3>Starboy</h3>
-          <p>The Weekend</p>
-        </div>
-        <p>4:16</p>
-      </div>
+
+      {data &&
+        data.map(({ id, title, artist, url, photo, duration }) => (
+          <div className="playerList-playlist">
+            <img alt="Album Poster" src={photo} />
+            <div className="playerList-album-title">
+              <h3>{title}</h3>
+              <p>{artist}</p>
+            </div>
+            <p>{duration}</p>
+          </div>
+        ))}
     </div>
   );
 };
