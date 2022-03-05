805a4ae7506d09792e49160f95f36a44b3560b41
Deepak Kamat <dkamat.001@gmail.com>
2022-03-05 20:39:40 +0530

    style: add css for music player

--- src/components/musicPlayer/MusicPlayer.js
+++ src/components/musicPlayer/MusicPlayer.js
@@ -1,7 +1,17 @@
+import "./MusicPlayer.css";
+import Cover from "./Cover.jpg";
+
 export const MusicPlayer = () => {
   return (
     <div className="musicPlayer">
-      <h3>MusicPlayer Component</h3>
+      <div className="musicPlayer-title">
+        <h3>Starboy</h3>
+        <p>The Weekend</p>
+      </div>
+      <div className="musicPlayer-image">
+        <img src={Cover} alt="Cover Image" />
+      </div>
+      <div className="musicPlayer-buttons"></div>
     </div>
   );
 };
