9901eda511fcd7cdae532a04faeea441af335dd7
Deepak Kamat <dkamat.001@gmail.com>
2022-03-05 21:02:56 +0530

    feat: integrate graphql with react

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom";
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 
 import App from "./App";
+
+const client = new ApolloClient({
+  uri: "https://api.ss.dev/resource/api",
+  cache: new InMemoryCache(),
+});
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+    ,
   </StrictMode>,
   rootElement
 );
