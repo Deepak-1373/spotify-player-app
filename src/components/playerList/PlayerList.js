50633045b7ee0b0340370283237eb706ba1ef36d
Deepak Kamat <dkamat.001@gmail.com>
2022-03-05 19:32:13 +0530

    feat: add playlist style

--- src/components/playerList/PlayerList.js
+++ src/components/playerList/PlayerList.js
@@ -1,14 +1,15 @@
 import "./PlayerList.css";
+import Album from "./album.png";
 
 export const PlayerList = () => {
   return (
     <div className="playerList">
       <div className="playerList-searchbar">
         <h2>For You</h2>
-        <input type="search" placeholder="Search Song, Artist" />
+        <input type="text" placeholder="Search Song, Artist" />
       </div>
       <div className="playerList-playlist">
-        <img alt="Album Poster" src="../sidebar/Vector.png" />
+        <img alt="Album Poster" src={Album} />
         <div className="playerList-album-title">
           <h3>Starboy</h3>
           <p>The Weekend</p>
