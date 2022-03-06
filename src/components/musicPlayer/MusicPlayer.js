c6a3bfce9851d4629418eade8ddb6ea60918ff1d
Deepak Kamat <dkamat.001@gmail.com>
2022-03-06 14:41:30 +0530

    feat: add play buttons

--- src/components/musicPlayer/MusicPlayer.js
+++ src/components/musicPlayer/MusicPlayer.js
@@ -1,5 +1,7 @@
 import "./MusicPlayer.css";
 import Cover from "./Cover.jpg";
+import Vector from "./Vector.png";
+import Ellipse from "./Ellipse 3.png";
 
 export const MusicPlayer = () => {
   return (
@@ -11,7 +13,15 @@ export const MusicPlayer = () => {
       <div className="musicPlayer-image">
         <img src={Cover} alt="Cover Image" />
       </div>
-      <div className="musicPlayer-buttons"></div>
+      <div className="musicPlayer-buttons">
+        <img src={Ellipse} alt="Ellipse Logo" className="ellipse-logo"></img>
+        <img src={Vector} alt="Vector Logo" className="vector-logo"></img>
+        <div className="playPause-div">
+          <img />
+          <img />
+          <img />
+        </div>
+      </div>
     </div>
   );
 };
